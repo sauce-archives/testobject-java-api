c8c61b8c7a0aa3fd557b068739c637238546c1b9
umutuzgur <umut.uzgur@ozu.edu.tr>
2016-06-03 12:16:32 +0200

    Bugfix and new fields added

--- src/main/java/org/testobject/rest/api/TestSuiteResource.java
+++ src/main/java/org/testobject/rest/api/TestSuiteResource.java
@@ -1,57 +1,69 @@
 package org.testobject.rest.api;
 
-
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("users/{user}/projects/{project}/batches")
 public interface TestSuiteResource {
 
-    class InstrumentationTestSuiteRequest {
+	class InstrumentationTestSuiteRequest {
 
-        @JsonProperty
-        public String appUploadId;
+		@JsonProperty
+		public String appUploadId;
 
-        @JsonProperty
-        public String testUploadId;
+		@JsonProperty
+		public String testUploadId;
 
-        @JsonProperty
-		public Boolean runAsPackage;
+		@JsonProperty
+		public List<String> methodsToRun;
 
-        @SuppressWarnings("unused")
-        private InstrumentationTestSuiteRequest() {
-        }
+		@JsonProperty
+		public List<String> classesToRun;
 
-        public InstrumentationTestSuiteRequest(String appUploadId, String testUploadId) {
-            this.appUploadId = appUploadId;
-            this.testUploadId = testUploadId;
-        }
+		@JsonProperty
+		public List<String> annotationsToRun;
 
-        public InstrumentationTestSuiteRequest(boolean runAsPackage) {
+		@JsonProperty
+		public List<String> sizesToRun;
+
+		@JsonProperty
+		public boolean runAsPackage;
+
+		@SuppressWarnings("unused")
+		private InstrumentationTestSuiteRequest() {
+		}
+
+		public InstrumentationTestSuiteRequest(String appUploadId, String testUploadId) {
+			this.appUploadId = appUploadId;
+			this.testUploadId = testUploadId;
+		}
+
+		public InstrumentationTestSuiteRequest(boolean runAsPackage) {
 			this.runAsPackage = runAsPackage;
 
-        }
+		}
 
+	}
 
-    }
+	@PUT
+	@Path("instrumentation/{testSuite}")
+	public void updateInstrumentationTestSuite(@PathParam("user") String user, @PathParam("project") String project,
+			@PathParam("testSuite") long testSuite,
+			InstrumentationTestSuiteRequest request);
 
-    @PUT
-    @Path("instrumentation/{testSuite}")
-    public void updateInstrumentationTestSuite(@PathParam("user") String user, @PathParam("project") String project,
-                                               @PathParam("testSuite") long testSuite,
-                                               InstrumentationTestSuiteRequest request);
+	@POST
+	@Path("instrumentation/{testSuite}/replay")
+	public long runInstrumentationTestSuite(@PathParam("user") String user, @PathParam("project") String project,
+			@PathParam("testSuite") long testSuite);
 
-    @POST
-    @Path("instrumentation/{testSuite}/replay")
-    public long runInstrumentationTestSuite(@PathParam("user") String user, @PathParam("project") String project, @PathParam("testSuite") long testSuite);
-
-    @PUT
-    @Path("instrumentation/newSuite/{testSuite}")
-    public Long createInstrumentationTestSuite(@PathParam("user") String user, @PathParam("project") String project,
-                                               @PathParam("testSuite") long testSuite,
-                                               InstrumentationTestSuiteRequest request);
+	@PUT
+	@Path("instrumentation/newSuite/{testSuite}")
+	public Long createInstrumentationTestSuite(@PathParam("user") String user, @PathParam("project") String project,
+			@PathParam("testSuite") long testSuite,
+			InstrumentationTestSuiteRequest request);
 }
