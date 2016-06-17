c874b1d72f811823aac68cc38b4cd0f2b8d3bc6d
abdulla <ahawara@testobject.com>
2016-06-17 10:37:20 +0200

    cleanup

--- src/main/java/org/testobject/rest/api/appium/common/data/Id.java
+++ src/main/java/org/testobject/rest/api/appium/common/data/Id.java
@@ -1,49 +1,46 @@
 package org.testobject.rest.api.appium.common.data;
 
-
-import com.google.common.base.Preconditions;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.common.base.Preconditions;
 
 public abstract class Id<T> {
 
-    private final T value;
+	private final T value;
 
-    protected Id(T value) {
-        Preconditions.checkNotNull(value);
-        this.value = value;
-    }
+	protected Id(T value) {
+		Preconditions.checkNotNull(value);
+		this.value = value;
+	}
 
-    @JsonValue
-    public T value() {
-        return value;
-    }
+	@JsonValue
+	public T value() {
+		return value;
+	}
 
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        @SuppressWarnings("unchecked")
-        Id<T> other = (Id<T>) obj;
-        if (value == null) {
-            if (other.value != null)
-                return false;
-        } else if (!value.equals(other.value))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
 
-    @Override
-    public String toString() {
-        return String.valueOf(value);
-    }
+		@SuppressWarnings("unchecked")
+		Id<T> other = (Id<T>) obj;
+		if ((value == null && other.value != null) || !value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(value);
+	}
 
 }
