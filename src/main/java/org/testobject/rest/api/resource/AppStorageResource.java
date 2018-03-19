9f8a9e6028f841c0e061effb4b5f0f0fd9249d4c
danielpaulus <daniel.paulus@icloud.com>
2018-03-19 10:51:14 +0100

    fixed upload

--- src/main/java/org/testobject/rest/api/resource/AppStorageResource.java
+++ src/main/java/org/testobject/rest/api/resource/AppStorageResource.java
@@ -1,13 +1,11 @@
 package org.testobject.rest.api.resource;
 
-import org.glassfish.jersey.media.multipart.Boundary;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
-import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
-
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class AppStorageResource {
 
@@ -19,17 +17,17 @@ public class AppStorageResource {
 
 	public String uploadAppXcuiTest(String apiKey, File ipa) {
 		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
-		FormDataMultiPart form = new FormDataMultiPart();
-		form.bodyPart(new FileDataBodyPart("file", ipa, MediaType.APPLICATION_OCTET_STREAM_TYPE));
-		MediaType contentType = MediaType.MULTIPART_FORM_DATA_TYPE;
-		contentType = Boundary.addBoundary(contentType);
-
-		return target
-				.path("storage").path("upload")
-				.request()
-				.header("Authorization", authorizationHeaderValue)
-				.header("App-Type", "XCUITEST")
-				.post(Entity.entity(form, contentType), String.class);
+		try {
+			return target
+					.path("storage").path("upload")
+					.request()
+					.header("Authorization", authorizationHeaderValue)
+					.header("App-Type", "XCUITEST")
+					.post(Entity.entity(Files.newInputStream(ipa.toPath()), MediaType.APPLICATION_OCTET_STREAM), String.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		throw new RuntimeException();
 	}
 
 }
