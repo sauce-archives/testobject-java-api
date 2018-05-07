389ff334c7e1f80867e5546bb03a880f4f004ace
David Missmann <david.missmann@saucelabs.com>
2018-05-07 15:33:38 +0200

    Extracted the API key conversion to an own method

--- src/main/java/org/testobject/rest/api/resource/v2/InstrumentationResourceImpl.java
+++ src/main/java/org/testobject/rest/api/resource/v2/InstrumentationResourceImpl.java
@@ -19,8 +19,7 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 
 	@Override
 	public StartInstrumentationResponse createAndStartXCUITestInstrumentation(String apiKey, StaticInstrumentationRequestData requestData) {
-		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
-
+		String authorizationHeaderValue = getAuthorizationHeaderValue(apiKey);
 		return target
 				.path("v2").path("instrumentation").path("xcuitest")
 				.request(MediaType.APPLICATION_JSON)
@@ -29,9 +28,9 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 	}
 
 	@Override
-	public StartInstrumentationResponse createAndStartXCUITestInstrumentation(String apiKey, DynamicInstrumentationRequestData requestData) {
-		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
-
+	public StartInstrumentationResponse createAndStartXCUITestInstrumentation(String apiKey,
+			DynamicInstrumentationRequestData requestData) {
+		String authorizationHeaderValue = getAuthorizationHeaderValue(apiKey);
 		return target
 				.path("v2").path("instrumentation").path("xcuitest").path("dynamic")
 				.request(MediaType.APPLICATION_JSON)
@@ -41,8 +40,7 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 
 	@Override
 	public StartInstrumentationResponse createAndStartAndroidInstrumentation(String apiKey, StaticInstrumentationRequestData requestData) {
-		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
-
+		String authorizationHeaderValue = getAuthorizationHeaderValue(apiKey);
 		return target
 				.path("v2").path("instrumentation").path("android")
 				.request(MediaType.APPLICATION_JSON)
@@ -50,10 +48,10 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 				.post(Entity.json(requestData), StartInstrumentationResponse.class);
 	}
 
-	@Override public StartInstrumentationResponse createAndStartAndroidInstrumentation(String apiKey,
+	@Override
+	public StartInstrumentationResponse createAndStartAndroidInstrumentation(String apiKey,
 			DynamicInstrumentationRequestData requestData) {
-		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
-
+		String authorizationHeaderValue = getAuthorizationHeaderValue(apiKey);
 		return target
 				.path("v2").path("instrumentation").path("android").path("dynamic")
 				.request(MediaType.APPLICATION_JSON)
@@ -63,8 +61,7 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 
 	@Override
 	public String getJUnitReport(String apiKey, long reportId) {
-		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
-
+		String authorizationHeaderValue = getAuthorizationHeaderValue(apiKey);
 		return target
 				.path("v2").path("instrumentation").path("testreport").path(Long.toString(reportId)).path("junitreport")
 				.request(MediaType.APPLICATION_XML)
@@ -74,7 +71,7 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 
 	@Override
 	public InstrumentationReport getTestReport(String apiKey, long reportId) {
-		String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
+		String authorizationHeaderValue = getAuthorizationHeaderValue(apiKey);
 		return target
 				.path("v2").path("instrumentation").path("testreport").path(Long.toString(reportId))
 				.request(MediaType.APPLICATION_JSON)
@@ -82,4 +79,7 @@ public class InstrumentationResourceImpl implements InstrumentationResource {
 				.get(InstrumentationReport.class);
 	}
 
+	private String getAuthorizationHeaderValue(String apiKey) {
+		return "Basic " + java.util.Base64.getEncoder().encodeToString(("user" + ":" + apiKey).getBytes());
+	}
 }
