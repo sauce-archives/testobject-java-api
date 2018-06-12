38aa6ffaec41ec1f1f0943891552eb659ea7ce59
abdulla <abdulla.hawara@saucelabs.com>
2018-06-12 18:51:51 +0200

    remove quality reports support; use v2 of videoResource and testReportResource

--- src/main/java/org/testobject/rest/api/resource/v2/ApiTestReportResource.java
+++ src/main/java/org/testobject/rest/api/resource/v2/ApiTestReportResource.java
@@ -1,4 +1,4 @@
-package org.testobject.rest.api.resource;
+package org.testobject.rest.api.resource.v2;
 
 import org.testobject.rest.api.model.TestReportWithDevice;
 
@@ -7,19 +7,18 @@ import javax.ws.rs.client.WebTarget;
 import static java.util.Base64.getEncoder;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 
-public class TestReportResource {
+public class ApiTestReportResource {
 
 	private final WebTarget target;
 
-	public TestReportResource(WebTarget target) {
+	public ApiTestReportResource(WebTarget target) {
 		this.target = target;
 	}
 
-	public TestReportWithDevice getTestReport(String userId, String projectId, long reportId, String apiKey) {
+	public TestReportWithDevice getTestReport(long reportId, String apiKey) {
 		String apiKeyHeader = "Basic " + getEncoder().encodeToString((":" + apiKey).getBytes());
 		return target
-				.path("users").path(userId)
-				.path("projects").path(projectId)
+				.path("v2")
 				.path("reports").path(Long.toString(reportId))
 				.request(APPLICATION_JSON_TYPE)
 				.header("Authorization", apiKeyHeader)
